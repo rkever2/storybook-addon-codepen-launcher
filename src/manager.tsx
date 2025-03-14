fe7e238856dda5f9ecf142379981d7b200371c6a
rkever2 <168228240+rkever2@users.noreply.github.com>
2025-03-14 16:27:23 -0700

    only show addon for story

--- src/manager.tsx
+++ src/manager.tsx
@@ -5,10 +5,11 @@ import { ADDON_ID, TOOL_ID } from "./constants";
 
 // Register the addon
 addons.register(ADDON_ID, (api) => {
-  // Register a tool
-  addons.add(TOOL_ID, {
-    type: types.TOOL,
-    title: "Codepen Addon",
-    render: () => <CodepenTool api={api} />,
-  });
+    // Register a tool
+    addons.add(TOOL_ID, {
+        type: types.TOOL,
+        title: "Codepen Addon",
+        match: ({ viewMode }) => viewMode === "story",
+        render: () => <CodepenTool api={api} />,
+    });
 });
--- src/withGlobals.ts
+++ src/withGlobals.ts
@@ -1,5 +1,4 @@
 import { useEffect, useGlobals } from "storybook/internal/preview-api";
-import { global } from "@storybook/global";
 import type { Renderer, StoryContext, PartialStoryFn as StoryFunction } from "storybook/internal/types";
 import { codepenEffect } from "./components/codepen/CodepenUseEffect";
 import { KEY } from "./constants";
@@ -7,7 +6,6 @@ import { KEY } from "./constants";
 export const withGlobals = (StoryFn: StoryFunction<Renderer>, context: StoryContext<Renderer>) => {
     const [globals, updateGlobals] = useGlobals();
     const codepenLauncherAddon = globals[KEY];
-    const canvas = context.canvasElement as ParentNode;
 
     // Is the addon being used in the docs panel
     const isInDocs = context.viewMode === "docs";
